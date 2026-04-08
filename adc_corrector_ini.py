0632240f10e28942c89befafe38edf06f7f121aa
Artem_student <ocnovnoy.exp@gmail.com>
2026-04-08 15:49:52 +0500

    feat: Добавлен try, except для ошибок, и задан timeout

--- adc_corrector_ini.py
+++ adc_corrector_ini.py
@@ -1,9 +1,13 @@
 import pyvisa
-rm = pyvisa.ResourceManager()
-#print(rm.list_resources('TCPIP?*')) # '?*' это фильтр необходимый для обнаружения нашего адреса устройства, так как бех него будут искаться только приборы, чьи имена заканчиваются на ::INSTR, а наше заканчивется на ::SOCKET
+rm = pyvisa.ResourceManager() # '@py' необходим для принудительного использования бэкенда не NI-VISA, а pyvisa-py
+#print(rm.list_resources('TCPIP?*')) '?*' это фильтр необходимый для обнаружения нашего адреса устройства, так как бех него будут искаться только приборы, чьи имена заканчиваются на ::INSTR, а наше заканчивется на ::SOCKET
 # Если мы хотим только ::SOCKET, то испольхзуем вот такой фильтр 'TCPIP?*'
-my_instrument = rm.open_resource('TCPIP0::localhost::5025::SOCKET')
-my_instrument.write_termination = '\n'   # Отправлять \n после каждой команды
-my_instrument.read_termination = '\n'    # Ждать \n в конце ответа
-my_instrument.write("*IDN?")
-print(my_instrument.read())
+try:
+    my_instrument = rm.open_resource('TCPIP0::localhost::5025::SOCKET')
+    my_instrument.timeout = 5000
+    my_instrument.write_termination = '\n'   # Отправлять \n после каждой команды
+    my_instrument.read_termination = '\n'    # Ждать \n в конце ответа
+    print(my_instrument.query("*IDN?"))
+except pyvisa.errors.VisaIOError as e:
+    print("Ошибка связанная с портом, pyvisa или чем то подобным")
+my_instrument.close()
