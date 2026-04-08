57daebb58bda97e6b5eff0c8baedcfa80b4f3d3e
Artem_student <ocnovnoy.exp@gmail.com>
2026-04-08 16:02:47 +0500

    feat: Добавлена команда syst:err?

--- adc_corrector_ini.py
+++ adc_corrector_ini.py
@@ -1,6 +1,6 @@
 import pyvisa
 rm = pyvisa.ResourceManager() # '@py' необходим для принудительного использования бэкенда не NI-VISA, а pyvisa-py
-#print(rm.list_resources('TCPIP?*')) '?*' это фильтр необходимый для обнаружения нашего адреса устройства, так как бех него будут искаться только приборы, чьи имена заканчиваются на ::INSTR, а наше заканчивется на ::SOCKET
+print(rm.list_resources('TCPIP?*')) #'?*' это фильтр необходимый для обнаружения нашего адреса устройства, так как бех него будут искаться только приборы, чьи имена заканчиваются на ::INSTR, а наше заканчивется на ::SOCKET
 # Если мы хотим только ::SOCKET, то испольхзуем вот такой фильтр 'TCPIP?*'
 try:
     my_instrument = rm.open_resource('TCPIP0::localhost::5025::SOCKET')
@@ -8,6 +8,7 @@ try:
     my_instrument.write_termination = '\n'   # Отправлять \n после каждой команды
     my_instrument.read_termination = '\n'    # Ждать \n в конце ответа
     print(my_instrument.query("*IDN?"))
+    print(my_instrument.query("syst:err?"))
 except pyvisa.errors.VisaIOError as e:
     print("Ошибка связанная с портом, pyvisa или чем то подобным")
 my_instrument.close()
