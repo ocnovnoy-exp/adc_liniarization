efa9cc7cc3ad6afdb98c7b32d5d30bec6c134f9e
Artem_student <ocnovnoy.exp@gmail.com>
2026-04-09 11:10:38 +0500

    refactor: Изменена функция для инициализации Теперь она возвращает 1 или 0, в зависимости от подключенного устройства(device, generator)

--- adc_corrector_ini.py
+++ adc_corrector_ini.py
@@ -4,20 +4,24 @@ print(rm.list_resources('TCPIP?*')) #'?*' это фильтр необходим
 # Если мы хотим только ::SOCKET, то испольхзуем вот такой фильтр 'TCPIP?*'
 
 list_of_adc = ["SN9000-10"] #Список для устройств которые являются device, а не generator, и это значит мы должны спрашивать количество их портов
-def initialization(instrument_adres):
-    print(instrument_adres.query("*IDN?"))
-    if instrument_adres.query("*IDN?").split(", ")[1] in list_of_adc: #Тут мы разделяем строку которую нам возвращает *IDN?, и смотрим что это за устройство
-        quantity_of_ports = instrument_adres.query(":SERV:PORT:COUN?")
+def initialization(instrument_adres):#Функиция для инициализации приборов
+    instrument = rm.open_resource(instrument_adres)
+    instrument.timeout = 5000
+    instrument.write_termination = '\n'   # Отправлять \n после каждой команды
+    instrument.read_termination = '\n'    # Ждать \n в конце ответа
+    print(instrument.query("*IDN?"))
+    if instrument.query("*IDN?").split(", ")[1] in list_of_adc: #Тут мы разделяем строку которую нам возвращает *IDN?, и смотрим что это за устройство
+        quantity_of_ports = instrument.query(":SERV:PORT:COUN?")
         print(quantity_of_ports)
-    print(instrument_adres.query("syst:err?"))
-    return
+        print(instrument.query("syst:err?"))
+        instrument.close()
+        return 1 #Если это device, то вернётся 1
+    print(instrument.query("syst:err?"))
+    instrument.close()
+    return 0 #Если это generator, то вернётся 0
 
 try:
-    my_instrument = rm.open_resource('TCPIP0::localhost::5025::SOCKET')
-    my_instrument.timeout = 5000
-    my_instrument.write_termination = '\n'   # Отправлять \n после каждой команды
-    my_instrument.read_termination = '\n'    # Ждать \n в конце ответа
-    initialization(my_instrument)
+    initialization('TCPIP0::localhost::5025::SOCKET')
 except pyvisa.errors.VisaIOError as e:
     print("Ошибка связанная с портом, pyvisa или чем то подобным")
-#my_instrument.close()
+
