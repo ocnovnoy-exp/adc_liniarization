20fcfc5a02582afde75a787709bbeeb56b318bea
Artem_student <ocnovnoy.exp@gmail.com>
2026-04-10 15:25:22 +0500

    feat: Добавленна функция swithing_t_r

--- adc_corrector_ini.py
+++ adc_corrector_ini.py
@@ -26,8 +26,34 @@ def initialization():#Функиция для инициализации при�
     print(generator.query("syst:err?"))
     return 
 
-#def device_setup(device_adres, generator_adres):
-#Для функции инициализации нужно передать либо адреса устройств, либо же передать уже открытые соединения, но тогда следует сделать отдельную функцию, которая бы говорила что хто за устройство
+list_of_segment_data = [5,0,1,0,0,0,2,936000000,936000000,2,1000,936000000,936000000,2,10000]
+def device_setup():
+    device.query("syst:pres")
+    device.query("trigger:source BUS")
+    device.query("init:cont 1")
+    device.query("trigger:wait WAIT")
+    device.query("sens:rosc:sour EXT")
+    device.query("SENS:SWE:TYPE SEGM")
+    device.query(f"SENS:SEGM:DATA {list_of_segment_data}")
+    device.query("trig:sing")
+
+generator_port = "R2"
+device_port = "T1"
+def swithing_t_r(latest_device_port):#latest_device_port это T ИЛИ R n-ого порядка до котрого мы доходим, задаётся оно в настройках программы либо в ini файле для каждого device
+    if device_port == latest_device_port:#Функция swithing_t_r нужна для переключения T и R котроые мы ихмеряем, то есть мы идем для device T1, R1, T2, R2 и тд, а для generator R2, T2, R2, T2 и тд
+        return
+    generator.query("syst:pres")
+    generator.query(f"calc:par1:def {generator_port}")
+    generator.query("calc:par1:spor 2")
+    device.query("calc:par1:spor 2")
+    device.query(f"calc:par1:def {device_port}")
+    device.query("calc:par1:spor 2")
+    if generator_port[0] == "R" and device_port[0] == "T":
+        generator_port = "T2"
+        device_port = f"R{device_port[1]}"
+    else:
+        generator_port = "R2"
+        device_port = f"T{int(device_port[1]) + 1}"
 
 try:
     device_or_generator_func('TCPIP0::localhost::5026::SOCKET')
@@ -49,28 +75,35 @@ except pyvisa.errors.VisaIOError as e:
 # SN9000-10 15.866128s init:cont 1
 # SN9000-10 15.866140s trigger:wait WAIT
 # SN9000-10 15.866152s sens:rosc:sour EXT
+
 # Obzor804 15.894331s syst:err?
 # Obzor804 15.896987s 0,"No error"
+
 # SN9000-10 16.374300s SENS:SWE:TYPE SEGM
 # SN9000-10 16.374401s SENS:SEGM:DATA 5,0,1,0,0,0,2,936000000,936000000,2,1000,936000000,936000000,2,10000
 # SN9000-10 16.876104s trig:sing
 # SN9000-10 16.876177s *OPC?
 # SN9000-10 16.891749s 1
+
 # Obzor804 21.871429s syst:pres
 # Obzor804 21.871487s calc:par1:def R2
 # Obzor804 21.871500s calc:par1:spor 2
+
 # SN9000-10 22.338480s syst:pres
 # SN9000-10 22.338528s calc:par1:def T1
 # SN9000-10 22.338542s calc:par1:spor 2
+
 # Obzor804 22.370561s trigger:source BUS
 # Obzor804 22.370599s init:cont 1
 # Obzor804 22.370612s trigger:wait WAIT
 # Obzor804 22.370624s outp:state 1
+
 # SN9000-10 22.838235s sens:rosc:sour EXT
 # SN9000-10 22.838270s outp:state 1
 # SN9000-10 22.838281s trigger:source BUS
 # SN9000-10 22.838293s init:cont 1
 # SN9000-10 22.838303s trigger:wait WAIT
+
 # SN9000-10 22.838315s serv:rec:corr:state 0
 # SN9000-10 22.838351s serv:rec:corr:state?
 # SN9000-10 22.943659s 0
