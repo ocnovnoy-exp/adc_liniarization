01fefd3308f2e7044d1fff9cee57ac697df9a149
Artem_student <ocnovnoy.exp@gmail.com>
2026-04-17 15:56:05 +0500

    feat: Добавил функцию для перебора мощностей Функция перебирает мощности и записывает значения с обоих устройств

--- adc_corrector_ini.py
+++ adc_corrector_ini.py
@@ -19,7 +19,7 @@ def device_or_generator_func(adres):#Эта функция нам нужна т�
     instrument.close()
     return 
 
-def load_ini(path: str):
+def load_ini(path: str):#Нужно добавить снятие данных о том какие T и R нужно смотреть
     cfg = ConfigParser()
     cfg.optionxform = str
     cfg.read(path, encoding="utf-8")
@@ -104,17 +104,27 @@ def switching_t_r(port):
     device.write("serv:rec:lin:state 0")# Если запускать программу в режиме проверки, то тут будет не 0, а 1
     device.query("serv:rec:lin:state?")
 
-power_up = -45.000000
-power_down = 10.000000 #Эти 2 значения тоже должны браться из ini файла
+def wait_opc(instrument):
+    answer_on_opc = instrument.query("*OPC?").strip()
+    if answer_on_opc != "1":
+        raise RuntimeError(f"Неожиданный ответ на *OPC?, а именно: {answer_on_opc}")
+
+def syst_err(instrument):
+    answer_on_opc = instrument.query("syst:err?").strip()
+    if answer_on_opc != '0,"No error"':
+        raise RuntimeError(f"Неожиданный ответ от syst:err?: {answer_on_opc}")
+
+power_down = -45.000000
+power_up = 10.000000 #Эти 2 значения тоже должны браться из ini файла
 def grid_of_powers(power_up: float, power_down: float, points_of_power: int = 125):#Изменение мощности происходит просто определением дельты, и уже после этого мы идем от наибольшей мозности отнимая значение дельты умноженной на номер шага 
     delta_power = (power_up - power_down) / (points_of_power - 1)
     return [power_up - delta_power * i for i in range(points_of_power)]
 
 def zone_for_point(num_of_point: int, ini_conf: dict):#Функця определяет какое bandwidth для этой точки(в каком из 3 сегментов находится точка)
     for zones_conf in ini_conf.get("zones"):#функция возвращает настройки для сегмента в котором находится точка
-        if num_of_point >= zones_conf.get("begin") and num_of_point < zones_conf.get("end"):
+        if zones_conf.get("begin") <= num_of_point < zones_conf.get("end"):
             return zones_conf
-        raise ValueError(f"Не найдена зона для точки {num_of_point}")
+    raise ValueError(f"Не найдена зона для точки {num_of_point}")
 
 #SENS:SEGM:DATA 100,10 или 3 зависит от ini файла, там 3 сегмента и для каждого своё значение
 def set_segment(instrument, frequency_hz: float, ifbw: int):
@@ -125,24 +135,42 @@ def set_segment(instrument, frequency_hz: float, ifbw: int):
         f"{frequency_hz:.0f},{frequency_hz:.0f},2,{ifbw},"
         f"{frequency_hz:.0f},{frequency_hz:.0f},2,{ifbw}"
     )
+    instrument.write("trig:sing")
+    wait_opc(instrument)
 
-#выбор того кто передаёт source1:power зависит от того кто является R
+def taking_fdat(instrument):#Эта функция берет список после CALC:DATA:FDAT?
+    syst_err(instrument)
+    instrument.write("calc:parameter1:select")
+    values = instrument.query("CALC:DATA:FDAT?")
+    return values
 
+def setting_scan_type(instrument, powers_grid: list, num_of_point: int, ini_conf: dict):#Функция необходимая для задания мощности и отправки SENS:SEGM:DATA 2 раза, с bandwidth = 1000 и той что мы берем их .ini файла
+    power = powers_grid[num_of_point]
+    zone = zone_for_point(num_of_point, ini_conf)
+    instrument.write(f"SOURce1:POWer {power:.6e}")
+    set_segment(instrument, ini_conf["frequency"], 1000)
+    set_segment(instrument, ini_conf["frequency"], zone["bandwidth"])
+
+def sens_data(instrument, power_up, power_down):#выбор того кому передаём source1:power зависит от того кто является R
+    power_grid = grid_of_powers(power_up, power_down, points_of_power=125)
+    for n in range(len(power_grid)):
+        setting_scan_type(instrument, power_grid, n, ini_config)
+        dev_val = taking_fdat(device)
+        gen_val = taking_fdat(generator)
 
 list_port = [[1, 1], [1, 1]] #Нужно добавить функцию для чтения ini файлов, из них в этот массив должны складываться какие R и T мы хотим посмотреть 
-def switching_port(list_port):#Функция swithing_port нужна для переключения T и R котроые мы ихмеряем, то есть мы идем для device T1, R1, T2, R2 и тд, а для generator R2, T2, R2, T2 и тд
+def switching_port(list_port):#Функция swithing_port нужна для переключения T и R котроые мы измеряем, то есть мы идем для device T1, R1, T2, R2 и тд, а для generator R2, T2, R2, T2 и тд
     for i in range(len(list_port)):
-        if list_port[i][0] == 1 or list_port[i][0] == 1:
-            input(f"Подключите 2 порт {generator_idn[1]} с {i} портом {device_idn[1]}, а после введите любой символ")
+        if list_port[i][0] == 1 or list_port[i][1] == 1:
+            input(f"Подключите 2 порт {generator_idn[1]} с {i + 1} портом {device_idn[1]}, а после введите любой символ")
         for j in range(len(list_port[i])):
             if list_port[i][j] == 1 and j == 0:
                 device_port = f"T{i + 1}"
                 switching_t_r(device_port)
             elif list_port[i][j] == 1 and j == 1:
                 device_port = f"R{i + 1}"   
-                switching_t_r(device_port) 
-
-    
+                switching_t_r(device_port)
+    return device_port
 
 try:
     device_or_generator_func('TCPIP0::localhost::5026::SOCKET')
@@ -156,7 +184,7 @@ try:
     generator.write_termination = '\n'   # Отправлять \n после каждой команды
     generator.read_termination = '\n'    # Ждать \n в конце ответа
     device_idn, generator_idn = initialization() 
-    ini_config = load_ini("C:\adc-corrector-develop\adc-corrector-develop\System\SN9000-10_2.ini")
+    ini_config = load_ini(r"C:\adc-corrector-develop\adc-corrector-develop\System\SN9000-10_2.ini")
 except pyvisa.errors.VisaIOError as e:
     print("Ошибка связанная с портом, pyvisa или чем то подобным")
 
